36134dde62215d1838fe113b460fd29f52efe8fe
ehsanvds <50964411+ehsanvds@users.noreply.github.com>
2020-04-22 23:14:57 -0400

    Update general_functions.py

--- general_functions.py
+++ general_functions.py
@@ -8,13 +8,15 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
 
-#%% Other Functions
+#%% General Functions
+# making a list of files with a specific extension in a directory
 def filelist(path, ext):
     from os import listdir
     file = [f for f in listdir(path) if f.endswith(ext)]
     file.sort()
     return file
 
+# Visualize the original and augmented images for comoparison
 def visualize(original, augmented):
     plt.figure()
     plt.subplot(1,2,1)
@@ -24,6 +26,8 @@ def visualize(original, augmented):
     plt.title('Augmented Image')
     plt.imshow(augmented)
 
+# Normalizing values of a data frame
+# cat_columns is a list of categorical columns.
 def normalize(df,cat_columns):
     df_cat = pd.DataFrame()
     for i in cat_columns:
@@ -32,7 +36,8 @@ def normalize(df,cat_columns):
     df = pd.concat([df_cat, df], axis=1)
     return df
 
-def read_image(file):
+# Reading a signel image
+def read_image(file, des_img_size):
     img_string = tf.io.read_file(file)
     image = tf.io.decode_image(img_string, channels=3)
     image = tf.image.convert_image_dtype(image, tf.float32)
