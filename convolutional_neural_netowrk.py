15c70f4c820bffeda1619401180e976aac940c30
ehsanvds <50964411+ehsanvds@users.noreply.github.com>
2020-05-01 12:34:30 -0400

    Update convolutional_neural_netowrk.py

--- convolutional_neural_netowrk.py
+++ convolutional_neural_netowrk.py
@@ -5,6 +5,7 @@ The neural network for imitation Learning
 import tensorflow as tf
 
 #%% Netwrok
+# Building a convolutional layer
 def conv_block(x, n_filters, krl_size, stride, drop_rate):
     layer = tf.keras.layers.Conv2D(n_filters, krl_size, stride, padding='same',
                                    activation='relu', use_bias=True)(x)
@@ -12,11 +13,13 @@ def conv_block(x, n_filters, krl_size, stride, drop_rate):
     layer = tf.keras.layers.Dropout(drop_rate)(layer)
     return layer
 
+# Building a fully connected layer
 def fc_block(x, n_units, drop_rate):
     layer = tf.keras.layers.Dense(n_units, activation='relu', use_bias=True)(x)
     layer = tf.keras.layers.Dropout(drop_rate)(layer)
     return layer
 
+# Building a convolutional network for images
 def img_network(x):
     # parameters: n_filters, krl_size, stride, drop_rate
     network1 = conv_block(x,         32, 5, 2, 0.5) # layer 1
@@ -31,13 +34,15 @@ def img_network(x):
     network1 = fc_block(network1, 512, 0.2)         # layer 9
     network1 = fc_block(network1, 512, 0.2)         # layer 10
     return network1
-    
+
+# Building a fully cocnnected network for measurements
 def msr_network(x):
     # parameters: n_units, drop_rate
     network2 = fc_block(x,        128, 0.2) # layer 1
     network2 = fc_block(network2, 128, 0.2) # layer 2
     return network2
 
+# Concatenating networks and building the full network
 def full_network(img_input, msr_input):
     img_module = img_network(img_input)    
     msr_module = msr_network(msr_input)
